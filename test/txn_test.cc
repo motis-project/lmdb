b1e9c35850b685d6deff9637a27fb5e5b6c7d759
Felix Gündling <felix.guendling@gmail.com>
2017-10-31 17:35:08 +0100

    more tests

--- test/txn_test.cc
+++ test/txn_test.cc
@@ -9,13 +9,26 @@ TEST_CASE("txn") {
   env.open("./");
 
   auto txn = lmdb::txn{env};
+  auto db = txn.dbi_open();
 
-  SUBCASE("dbi open works") { txn.dbi_open(); }
-
-  SUBCASE("put and retrieve") {
-    auto db = txn.dbi_open();
+  SUBCASE("put and get success") {
     txn.put(db, "key1", "hello world");
-    auto const v = txn.get(db, "key1");
-    CHECK(v == "hello world");
+    CHECK(txn.get(db, "key1") == "hello world");
+  }
+
+  SUBCASE("put and get no success (wrong key)") {
+    txn.put(db, "key1", "hello world");
+    CHECK(!txn.get(db, "key2"));
+  }
+
+  SUBCASE("put and get no success (deleted)") {
+    txn.put(db, "key1", "hello world");
+    txn.del(db, "key1");
+    CHECK(!txn.get(db, "key1"));
+  }
+
+  SUBCASE("delete no success") {
+    txn.put(db, "key2", "hello world");
+    CHECK(txn.del(db, "key1") == false);
   }
 }
