d8c468ee32bbf36c2c744ef3fc12e2accb816f88
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-05-22 09:46:14 +0200

    clang-tidy

--- include/lmdb/enum_helper.h
+++ include/lmdb/enum_helper.h
@@ -1,20 +1,24 @@
 // by Mahmoud Al-Qudsi
 // Source: https://softwareengineering.stackexchange.com/a/329443
 
-// NOLINTNEXTLINE(misc-macro-parentheses)
-#define ENUM_FLAG_OPERATOR(T, X)                                 \
-  inline T operator X(T lhs, T rhs) {                            \
-    return T(static_cast<std::underlying_type_t<T>>(lhs)         \
-                 X static_cast<std::underlying_type_t<T>>(rhs)); \
+#define ENUM_FLAG_OPERATOR(T, X)                                              \
+  inline T operator X(T lhs, T rhs) {                                         \
+    return T(static_cast<                                                     \
+             std::underlying_type_t<T /* NOLINT(misc-macro-parentheses) */>>( \
+        lhs)                                                                  \
+                 X static_cast<std::underlying_type_t<                        \
+                     T /* NOLINT(misc-macro-parentheses) */>>(rhs));          \
   }
 
 // NOLINTNEXTLINE(misc-macro-parentheses)
-#define ENUM_FLAGS(T)                                     \
-  enum class T : uint32_t;                                \
-  inline T operator~(T t) {                               \
-    return T(~static_cast<std::underlying_type_t<T>>(t)); \
-  }                                                       \
-  ENUM_FLAG_OPERATOR(T, |)                                \
-  ENUM_FLAG_OPERATOR(T, ^)                                \
-  ENUM_FLAG_OPERATOR(T, &)                                \
+#define ENUM_FLAGS(T)                                                         \
+  enum class T : uint32_t;                                                    \
+  inline T operator~(T t) {                                                   \
+    return T(~static_cast<                                                    \
+             std::underlying_type_t<T /* NOLINT(misc-macro-parentheses) */>>( \
+        t));                                                                  \
+  }                                                                           \
+  ENUM_FLAG_OPERATOR(T, |)                                                    \
+  ENUM_FLAG_OPERATOR(T, ^)                                                    \
+  ENUM_FLAG_OPERATOR(T, &)                                                    \
   enum class T : uint32_t
