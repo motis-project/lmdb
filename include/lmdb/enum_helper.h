2764b58701e630dc2d87ae2dfdfa2cebbdf6375d
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-05-22 09:41:36 +0200

    clang-tidy

--- include/lmdb/enum_helper.h
+++ include/lmdb/enum_helper.h
@@ -1,12 +1,14 @@
 // by Mahmoud Al-Qudsi
 // Source: https://softwareengineering.stackexchange.com/a/329443
 
+// NOLINTNEXTLINE(misc-macro-parentheses)
 #define ENUM_FLAG_OPERATOR(T, X)                                 \
   inline T operator X(T lhs, T rhs) {                            \
     return T(static_cast<std::underlying_type_t<T>>(lhs)         \
                  X static_cast<std::underlying_type_t<T>>(rhs)); \
   }
 
+// NOLINTNEXTLINE(misc-macro-parentheses)
 #define ENUM_FLAGS(T)                                     \
   enum class T : uint32_t;                                \
   inline T operator~(T t) {                               \
